6e10d9520349f151ea2391f375cce47d175e14ff
k4l4p <kalapwong@gmail.com>
2022-05-26 20:00:56 +0800

    first commit and hopfully have the second one

---
+++ README.md
@@ -0,0 +1,10 @@
+# share
+
+A (probably) file transfer app using (probably) webRTC (or purely peerJS)
+
+## Tasks
+
+- [] implement peerJS connection
+- [] file transfer while peerJS
+- [] webRTC server 
+- [] webRTC client
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import {Peer} from 'peerjs'
+
+let x = new Peer()
+</script>
+
+<template>
+
+  <div class="container mx-auto">
+    <button 
+    class="bg-sky-500 hover:bg-sky-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white"
+    id="start">
+    Start
+  </button>
+  </div>
+  
+</template>
+
