5dbfc6968d2e576c2d3ea9ac3935e4dce6db1ad6
k4l4p <kalapwong@gmail.com>
2022-05-27 13:32:59 +0800

    peerjs does not supprt vue3

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A (probably) file transfer app using (probably) webRTC (or purely peerJS)
 
 ## Tasks
 
-- [ ] implement peerJS connection
-- [ ] file transfer while peerJS
+- [ ] ~~implement peerJS connection~~
+- [ ] ~~file transfer while peerJS~~ (Vue3 doesn't support peerjs)
 - [ ] webRTC server 
 - [ ] webRTC client
--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <script setup>
+import { ref, watch, watchEffect } from 'vue'
 
-import {Peer} from 'peerjs'
+let p 
+let pass = "i_am_an_id"
 
-let x = new Peer()
+
+let start = () => {
+
+}
+
+let recv = () => {
+
+}
+
+
 </script>
 
 <template>
 
   <div class="container mx-auto">
     <button 
+    @click="start()"
     class="bg-sky-500 hover:bg-sky-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white"
     id="start">
     Start
   </button>
+  <button 
+    @click="recv"
+    class="bg-sky-500 hover:bg-sky-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white"
+    id="start">
+    Receive
+  </button>
   </div>
   
 </template>
-
-  
